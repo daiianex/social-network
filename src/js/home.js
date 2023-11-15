8cf8c16e2ceee5c7bd61d8f4422b5a82afc11619
Daiane <daiianeeg13@gmail.com>
2023-11-15 10:07:46 -0300

    pageHome

--- src/js/home.js
+++ src/js/home.js
@@ -3,13 +3,15 @@ export default () => {
     
     const templateHome = `
         <form class="container">
-            
-            <p>Sua rede de teorias  da conspiração
-            feita para compartilhar historias e 
-            visão sobre </p>
+            <img src="./img/logo.png" />
+            <section class="textAndbtn">
+                <p>Sua rede de teorias  da conspiração
+                feita para compartilhar historias e 
+                visão sobre </p>
 
-            
-            <button><a href="#login">ENTRAR</a></button>
+                <button class="btnStart"><a href="#login">ENTRAR</a></button>
+
+            </section>
             
         </form>
     `;
