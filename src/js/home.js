b2c7d2118c9704500401a7ad2559f83cbfc3a4a7
Daiane <daiianeeg13@gmail.com>
2023-11-15 20:05:48 -0300

    page register

--- src/js/home.js
+++ src/js/home.js
@@ -1,19 +1,19 @@
 export default () => {
     const containerHome = document.createElement("div");
-    
+
     const templateHome = `
         <section class="container">
-            <section class="content first-content">
+            <section class="content">
                 <section class="first-column">
                     <img class="logo" src="img/logo.png" alt="Logo">
                 </section>
                 <section class="second-column">
                     <p>Sua rede de teorias  da conspiração
-                    feita para compartilhar historias e 
+                    <br>feita para compartilhar historias e 
                     visão sobre </p>
                     <button class="btnPrincipal"><a href="#login">ENTRAR</a></button>
                 </section>
-            <section>    
+            </section>    
         </section>
     `;
     containerHome.innerHTML = templateHome;
--- src/js/register.js
+++ src/js/register.js
@@ -6,14 +6,19 @@ export default () => {
 
     const templateRegister = `
         <form class="container">
-            <section class="containerPrincipal">
-                <p class="loginParagraph">Entre na sua conta</p>
+            <section class="content">
+                <section class="first-column">
+                    <img class="logo" src="img/logo.png" alt="Logo">
+                </section>
+                <section class="second-column">
+                    <p class="loginParagraph">Entre na sua conta</p>
 
-                <input type="text" class="name" placeholder="Nome Completo" />
-                <input type="email" class="input Email" id="inputEmail" placeholder="Insira seu Email" /><br>
-                <input type="password" class="input Senha" id="inputSenha" placeholder="Insira sua senha" /><br>
-                <p id="message" class="successMessage menssage"></p>
-                <br><button class="btnPrincipal" id="btn-Register">Criar conta</button><br>
+                    <input type="text" class="name" id="inputName" placeholder="Nome Completo" />
+                    <input type="email" class="input Email" id="inputEmail" placeholder="Insira seu Email" /><br>
+                    <input type="password" class="input Senha" id="inputPassword" placeholder="Insira sua senha" /><br>
+                    <p id="message" class="successMessage menssage"></p>
+                    <br><button class="btnPrincipal" id="btn-Register">Criar conta</button><br>
+                </section>
             </section>
         </form>
     `;
@@ -21,12 +26,15 @@ export default () => {
     containerRegister.innerHTML = templateRegister;
 
     const btnSignUp = containerRegister.querySelector("#btn-Register");
+    const name = containerRegister.querySelector("#inputName");
+    const email = containerRegister.querySelector("#inputEmail");
+    const password = containerRegister.querySelector("#inputPassword");
 
     btnSignUp.addEventListener("click", (e) => {
         e.preventDefault();
         const validation = validatedMessage(name.value, email.value, password.value);
         if (validation !== "") {
-            const message = containerSignup.querySelector("#message");
+            const message = containerRegister.querySelector("#message");
             message.innerHTML = validation;
         } else {
             registerNewUser(name.value, email.value, password.value)
@@ -35,7 +43,7 @@ export default () => {
                 })
                 .catch((error) => {
                     const errorMessage = errorsFirebase(error.code);
-                    const message = containerSignup.querySelector("#message");
+                    const message = containerRegister.querySelector("#message");
                     message.innerHTML = errorMessage;
                 });
         }
